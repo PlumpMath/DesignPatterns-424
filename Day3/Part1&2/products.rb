27b463cfdadc7f7e3513c62ea2c20095ebdb9f18
Shruti <shrutigupta34@gmail.com>
2016-12-07 17:05:25 +0000

    Added inheritance for part1

--- Day3/Part1&2/products.rb
+++ Day3/Part1&2/products.rb
@@ -1,17 +1,22 @@
-class ProductA
+class Product
+  def do_my_job
+    puts "I didn't define my job. I am a lazy and shitty product"
+  end
+end
+class ProductA < Product
 
   def do_my_job
     puts "I'm a ProductA, doing my stuff"
   end
 end
 
-class ProductB
+class ProductB < Product
   def do_my_job
     puts "I'm a ProductB, doing it"
   end
 end
 
-class ProductC
+class ProductC < Product
   def do_my_job
     puts "I'm a ProductC, performing"
   end
